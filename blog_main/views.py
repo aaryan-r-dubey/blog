d30b359e730502f20676de340e98fd9dddb057e3
aaryan2604 <aaryan.dubey@somaiya.edu>
2025-11-05 23:54:03 +0530

    Initial commit

---
+++ blog_main/views.py
@@ -0,0 +1,69 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from blogs.models import Category,Blogs
+from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
+
+def home(request):
+    categories=Category.objects.all()
+    featured_post=Blogs.objects.filter(is_featured=True)
+    
+    posts=Blogs.objects.filter(is_featured=False, status='published')
+
+
+    context={
+        'categories':categories,
+        'featured_post':featured_post,
+        'posts':posts
+
+    }
+    # return HttpResponse("education for nation")
+    return render(request,'home.html',context)
+
+
+def register(request):
+    if request.method=="POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    else:
+        form=RegisterForm()
+    
+    context={
+        'form':form
+    }
+    return render(request,'register.html',context)
+
+
+#login
+def login(request):
+
+    if request.method=="POST":
+        form=AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            print("hello ",username)
+            print("this is the passweord",password)
+
+            user=auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+                return redirect('dashboard')
+    else:
+        form=AuthenticationForm()
+
+    
+    context={
+        'form':form
+    }
+
+    return render(request,'login.html',context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
